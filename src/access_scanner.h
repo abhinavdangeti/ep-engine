f119c0707d125c9ee6dc9017658b62ef07652ff9
Sundar Sridharan <sundar.sridharan@gmail.com>
2015-07-23 15:39:59 -0700

    Switch to hrtime from timeval in Global Thread Pool
    This has small improvements in memory and cpu usage.
    Also fixes several ThreadSanitizer races from unit tests.
    
    Change-Id: I78fdddb832251fc062058c04f75f8d22c4c2f68d
    Reviewed-on: http://review.couchbase.org/53381
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chiyoung Seo <chiyoung@couchbase.com>

--- src/access_scanner.h
+++ src/access_scanner.h
@@ -45,6 +45,8 @@ public:
     AtomicValue<size_t> completedCount;
 
 private:
+    void updateAlogTime(double sleepSecs);
+
     EventuallyPersistentStore &store;
     EPStats &stats;
     double sleepTime;
